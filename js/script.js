a241ee69fb7c6eba6e5930842d22d6759bb5203b
Jaivonah Adside <jjtheartist23@gmail.com>
2018-12-19 23:22:56 +0000

    session 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 /* global $ */
 
+
+// FOR NEXT TIME: WE COULD NOT FIX A BUG "$ IS NOT DEFINED". ASK ONE OF THE VOLUNTEERS!
+
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
@@ -10,23 +13,43 @@ $(document).ready(function() {
         var q3Result = $("#question3").val();
         var totalScore;
     });
-function hightop(q1R) {
+
+$("#message").html(sneakers("high", " no lace"," open toe"));
+
+});
+
+function sneakers(qr1, qr2, qr3 ){
+    var message= "your sneaker is a " + " " + qr1 + " " + qr2 + " " + qr3 + " " + "!";
+    return message;
+
+}
+
+ function hightop(q1R) {
     if (q1R=== "high")
     { 
         return 3; }
     else if (q1R=== "low")
-    { return 1; }
+    { return 1; 
+        
+    }
     
-    else if (q1R==="medium")
-    { return 2;}
-    return 0;
+    else if (q1R==="medium"){
+        return 2;
+    }
+        
+    else{ return 0;
  }
-});
+}
+
+
+console.log(hightop("low"));
 
 
 function expose (q2R) {
     if (q2R=== "close")
-    {return 5;}
+    {return 5;
+        
+    }
      
      else if (q2R==="open"){
          return 3;
@@ -38,27 +61,18 @@ function expose (q2R) {
      }
 }    
      
-function option (q3R){  
+function option(q3R){  
     if (q3R=== "laces")
     { return 4;
-}
+ }
     else if (q3R===" no laces") {
         return 2;
-}
-
-else { return 0;
-
-}
-
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+ else { return 0;
+
+ }
 }
+
+    
+
